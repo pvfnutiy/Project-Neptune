272580b0627c8c08003d896b658c99ecea91fa97
pafnutiy <105767751+pvfnutiy@users.noreply.github.com>
2024-11-12 03:13:20 +0700

    clearify code
    0 sense code is deleted

--- src/main.cpp
+++ src/main.cpp
@@ -29,21 +29,6 @@ int main() {
 	double lastTime = 0.0;
 	double currentTime;
 
-	const char* fShaderCode =
-		"#version 410 core\n"
-		"out vec4 FragColor;\n"
-		"void main() {\n"
-		"	FragColor = vec4(0.8, 1.0, 1.0, 1.0);\n"
-		"}\n";
-
-	const char* vShaderCode = 
-		"#version 410 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main() {\n"
-		"	gl_Position = vec4(aPos, 1.0);\n"
-		"}\n";
-
-	unsigned int fShader;
 	line newline(0, 0, 0.3f, 0.8f);
 
 	while (!glfwWindowShouldClose(window)) {
